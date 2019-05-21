9449dc7499ae45bdc55e3c2064beb6a6a84f00fe
Erik Bartoš <erik.bartos@gmail.com>
2019-05-21 13:47:30 +0200

    running programs

--- src/pymikor.py
+++ src/pymikor.py
@@ -1,38 +1,26 @@
-def f(x):
-    df = 0
-    for d in range(len(x)):
-        df += x[d]
-    return df
+import math
+import numpy as np
 
 
-def f2(x):
-    df = 0
-    for d in range(len(x)):
-        df += pow(x[d], 2)
-    return df
+class Mikor:
 
+    def __init__(self):
+        self.dimS = 4
+        self.dimN = 1
+        self.nodesN = 100
 
-def fmultiply(x):
-    df = 1
-    for d in range(len(x)):
-        df *= x[d]
-    return df
+    def set(self, dims, dimn, nodes):
+        """
+        Class Mikor
+        :param dimS: Dimension of integral
+        :param dimN: Dimension of result
+        :return:
+        """
+        self.dimS = dims
+        self.dimN = dimn
+        self.nodesN = nodes
 
-
-def integrator(f, t):
-    res = list(map(lambda x: x(t), f))
-    return res
-
-
-def main():
-    funcs = [f, f2, fmultiply]
-    t = [1, 3, 4, 5, 10]
-
-    result = integrator(funcs, t)
-
-    for i in range(len(result)):
-        print('%i.  %.2f' % (i + 1, result[i]))
-
-
-if __name__ == "__main__":
-    main()
+    def result(self):
+        print('dimension of integration:', self.dimS)
+        print('dimension of result     :', self.dimN)
+        print('number of nodes         :', self.nodesN)
---
+++ src/test_functions.py
@@ -0,0 +1,42 @@
+import math
+from pymikor import *
+
+
+def f(x):
+    df = 0
+    for d in range(len(x)):
+        df += x[d]
+    return df
+
+
+def f2(x):
+    df = 0
+    for d in range(len(x)):
+        df += pow(x[d], 2)
+    return df
+
+
+def fmultiply(x):
+    df = 1
+    for d in range(len(x)):
+        df *= x[d]
+    return df
+
+
+def integrator(f, t):
+    res = list(map(lambda x: x(t), f))
+    return res
+
+
+def main():
+    funcs = [f, f2, fmultiply]
+    t = [1, 3, 4, 5, 10]
+
+    result = integrator(funcs, t)
+
+    for i in range(len(result)):
+        print('%i.  %.2f' % (i + 1, result[i]))
+
+
+if __name__ == "__main__":
+    main()
