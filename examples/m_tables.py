6bed0226a4cfcedbbf579df809472c5fa8a17640
Erik Bartoš <erik.bartos@gmail.com>
2019-06-16 20:59:52 +0200

    m tables

--- examples/m_tables.py
+++ examples/m_tables.py
@@ -14,10 +14,10 @@ def main():
           [4, 307, 523, 701, 829, 1069, 1543, 2129, 3001, 4001, 4097, 5003, 6007, 6023, 8191, 10007, 12029, 24041],
           [5, 1069, 1543, 2129, 3001, 4001, 5003, 6007, 8191, 10007],
           [6, 2129, 3001, 4001, 5003, 6007, 6023, 8191, 10007, 12029, 24041])
-    pa = np.array([23])#, 53, 101, 151, 307])
     mn = np.array([23, 53, 101, 151, 307, 523, 829, 1259, 2129, 3001, 4001, 5003,
                    6007, 8191, 10007, 13001, 20011, 30011, 40009, 50021, 75011,
                    100003, 200003, 500009, 1000003, 2000003, 5000011])
+    mn = np.array([5003, 6007])
 
     integral = Mikor()
     # integral.show_parameters()
@@ -26,21 +26,22 @@ def main():
     with open('coefficients.txt', 'w') as f:
         f.write('Tables of optimal coefficients\n')
 
-    arr = pa
-    for i in range(3, 20):
+    arr = mn
+    for i in range(10, 11):
         print(str(i), sep=' ', end=' ', flush=True)
         with open('coefficients.txt', 'a') as f:
             f.write(f'\n s = {i}\n')
             f.write('  N=p' + ' ' * 7 + 'a_2 -->\n')
             f.write(' ' + '-' * 34 + '\n')
             for j in range(len(arr)):
+                print(j)
                 integral.set_values(i, arr[j], 2, 1, sigma=3)
                 mo, mo_val = integral.first_optimal_a()
                 f.write(f'{arr[j]:6}   ')
                 opt_a = integral.calc_optimal_coefficients_a(mo)
                 f.write(f'{mo:5}' + ' ' * 2)
-                # for k in range(2, len(opt_a)):
-                #    f.write(f'{opt_a[k]:5}' + ' ' * 2)
+                for k in range(2, len(opt_a)):
+                    f.write(f'{opt_a[k]:5}' + ' ' * 2)
                 f.write('\n')
     print('\n')
 
