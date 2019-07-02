1d1cbdacda17ebf25cef84472c52ad75863b9de9
Erik Bartoš <erik.bartos@gmail.com>
2019-07-02 14:30:33 +0200

    4. dimension

--- examples/find_optimal_a.py
+++ examples/find_optimal_a.py
@@ -19,17 +19,17 @@ def main():
 
     c = 'y'
     while c == 'y':
-        # p = input('\n Set p number: ')
-        p = int(500009)
+        # p = int(input('\n Set p number: '))
+        p = int(13001)
         if not is_prime(p):
             print(f'{p} in NOT prime number')
             continue
-        integral.set_dpq(3, p, 1)
+        integral.set_dpq(4, p, 1)
         integral.show_parameters()
         opt_a, opt_val = integral.first_optimal_a(1)
         print(f'First optimal = {opt_a}')
 
-        aoc = ([opt_a, 55279])
+        aoc = ([opt_a, 778])
         for i in range(len(aoc)):
             h_valueA = integral.h_poly(aoc[i])
             h_valueB = integral.h_poly_chet(aoc[i])
--- src/test_mikor.py
+++ src/test_mikor.py
@@ -33,7 +33,7 @@ def fcn3(x):
 
 def main():
     integral = Mikor()
-    integral.set_values(1, 3, 10000, 1, sigma=1)
+    integral.set_values(1, 4, 10000, 1, sigma=1)
     # integral.set_dpq(3, 907, 31)
     integral.show_parameters()
 
