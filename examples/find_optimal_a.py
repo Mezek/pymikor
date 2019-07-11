77c27394c6ee49963a732e67af48048c4a2dba89
Erik Bartoš <erik.bartos@gmail.com>
2019-07-11 16:43:37 +0200

    new values

--- examples/find_optimal_a.py
+++ examples/find_optimal_a.py
@@ -20,16 +20,16 @@ def main():
     c = 'y'
     while c == 'y':
         # p = int(input('\n Set p number: '))
-        p = int(13001)
+        p = int(30011)
         if not is_prime(p):
             print(f'{p} in NOT prime number')
             continue
-        integral.set_dpq(4, p, 1)
+        integral.set_dpq(5, p, 1)
         integral.show_parameters()
         opt_a, opt_val = integral.first_optimal_a(1)
         print(f'First optimal = {opt_a}')
 
-        aoc = ([opt_a, 778])
+        aoc = ([opt_a, 2311])
         for i in range(len(aoc)):
             h_valueA = integral.h_poly(aoc[i])
             h_valueB = integral.h_poly_chet(aoc[i])
