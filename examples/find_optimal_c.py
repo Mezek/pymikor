7e83017544556e02f4b4742fcdb36bc89f1c58c2
Erik Bartoš <erik.bartos@gmail.com>
2019-07-11 12:26:41 +0200

    4th dimension

--- examples/find_optimal_c.py
+++ examples/find_optimal_c.py
@@ -20,19 +20,20 @@ def main():
     c = 'y'
     while c == 'y':
         # p = int(input('\n Set p number: '))
-        p = int(907)
-        opt_a = 402
-        q = int(31)
+        qq = np.array([5003, 61, 2053])
+        p = qq[0]
+        q = qq[1]
+        opt_a = qq[2]
         if not is_prime(p):
-            print(f'{p} in NOT prime number')
-            continue
+            print(f'p={p} in NOT prime number')
+            break
         if not is_prime(q):
-            print(f'{q} in NOT prime number')
-            continue
-        integral.set_dpq(3, p, q)
+            print(f'q={q} in NOT prime number')
+            break
+        integral.set_dpq(4, p, q)
         integral.show_parameters()
         opt_b, opt_b_val = integral.first_optimal_b(opt_a)
-        print(f'First optimals = {opt_a} {opt_b}')
+        print(f'Optimals = {p}, {q}, {integral.a_opt}, {opt_b}')
 
         # c = input('\n Continue? [y/n]')
         c = 'n'
