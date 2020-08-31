3fa0cef64dd473bc17de94c963413f155437e5cf
Erik Bartoš <erik.bartos@gmail.com>
2020-08-31 15:39:57 +0200

    exact oscillatory

--- examples/test_genz_suit.py
+++ examples/test_genz_suit.py
@@ -7,34 +7,32 @@ __author__ = "Erik Bartoš"
 __copyright__ = "Copyright © 2020 Erik Bartoš"
 __email__ = "erik.bartos@gmail.com"
 
-import math
 from pymikor import *
 from integrand_suit import *
 
 
 def fcn_osc(a, u):
-    fu = math.cos(2.*math.pi*u[0] + math.fsum(a))
-    fd = math.cos(2.*math.pi*u[0])
-    print(fu, fd)
+    b = 2.*math.pi*u[0]
+    s = math.fsum(a)
     prod = 1.
-    for e in a:
-        print(e)
-        prod *= e
-    return (fu - fd)*prod
+    for ael in a:
+        prod = 2.*math.sin(ael/2.)/ael
+    return math.cos((2.*b + s)/2.)*prod
 
 
 def main():
     with open('coefficients.txt', 'w') as f:
         f.write('\n')
 
-    fof = Integrand('FCN', 4)
+    fof = Integrand('FCN', 5)
     a = fof.a
     u = fof.u
     integral = Mikor()
-    integral.set_values(1, 4, 10000, 1, sigma=2)
+    integral.set_values(2, 5, 1000, 1, sigma=2)
     integral.show_parameters()
     result = integral(fof.oscillatory_fcn)
     # integral.show_parameters()
+    print(a, u)
     print(f'\nResult of integration   : {result}, {fcn_osc(a, u)}')
 
     del integral
