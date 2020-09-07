2a1d93991297d8b3e1bfaf4a68b3311dd545accd
Erik Bartoš <erik.bartos@gmail.com>
2020-09-07 22:14:19 +0200

    scaling

--- examples/test_genz_suit.py
+++ examples/test_genz_suit.py
@@ -15,46 +15,79 @@ def main():
     with open('coefficients.txt', 'w') as f:
         f.write('Genz suit')
 
-    nd = (3, 5, 8, 10, 13, 15)
-    for i in nd:
-        with open('coefficients.txt', 'a') as f:
-            f.write('\nDimension: ' + str(i))
-
-    dmn = 15
     integral = Mikor()
-    integral.set_values(1, dmn, 1000, 1, sigma=2)
-    integral.show_parameters()
+    nd = (3, 5, 8, 10, 13, 15)
+    prnb = (1259, 10007, 100003, 1000003)
+    for idn in nd:
+        with open('coefficients.txt', 'a') as f:
+            f.write('\nDimension: ' + str(idn))
+        for nod in prnb:
+            integral.set_values(1, idn, nod, 1, sigma=2)
+            # integral.show_parameters()
+            fof = Integrand('FCN', idn)
+            # fof.normalize_a(2, 600)
+            with open('coefficients.txt', 'a') as f:
+                f.write(f'\nN={nod:12.0f}')
+                # fof.show_parameters()
+                f.write('   1:')
+                result = integral(fof.oscillatory_fcn)
+                exact_res = fof.exact_oscillatory()
+                diff_res = result - exact_res
+                f.write(f'{diff_res:.3e}')
+                print(f'\n1: Oscillatory:')
+                print(f'{result:.10e}')
+                print(f'{exact_res:.10e}')
+                print(f'{diff_res:.3e}')
 
-    fof = Integrand('FCN', dmn)
-    fof.normalize_a(2, 600)
-    fof.show_parameters()
+                f.write('   2:')
+                result = integral(fof.product_peak_fcn)
+                exact_res = fof.exact_product_peak()
+                diff_res = result - exact_res
+                f.write(f'{diff_res:.3e}')
+                print(f'\n2: Product peak:')
+                print(f'{result:.10e}')
+                print(f'{exact_res:.10e}')
+                print(f'{diff_res:.3e}')
 
-    result = integral(fof.oscillatory_fcn)
-    exact_res = fof.exact_oscillatory()
-    print(f'\nResults oscillatory:')
-    print(f'{result:.10e}')
-    print(f'{exact_res:.10e}')
-    print(f'{(result - exact_res):.3e}')
+                f.write('   3:')
+                result = integral(fof.gaussian_fcn)
+                exact_res = fof.exact_gaussian()
+                diff_res = result - exact_res
+                f.write(f'{diff_res:.3e}')
+                print(f'\n3: Gaussian:')
+                print(f'{result:.10e}')
+                print(f'{exact_res:.10e}')
+                print(f'{diff_res:.3e}')
 
-    # result = integral(fof.product_peak_fcn)
-    # exact_res = fof.exact_product_peak()
-    # print(f'\nResults: {result} {exact_res} {result - exact_res}')
+                f.write('   4:')
+                result = integral(fof.c0_fcn)
+                exact_res = fof.exact_c0()
+                diff_res = result - exact_res
+                f.write(f'{diff_res:.3e}')
+                print(f'\n4: C0:')
+                print(f'{result:.10e}')
+                print(f'{exact_res:.10e}')
+                print(f'{diff_res:.3e}')
 
-    # result = integral(fof.gaussian_fcn)
-    # exact_res = fof.exact_gaussian()
-    # print(f'\nResults: {result} {exact_res} {result - exact_res}')
+                f.write('   5:')
+                result = integral(fof.corner_peak_fcn)
+                exact_res = fof.exact_corner_peak()
+                diff_res = result - exact_res
+                f.write(f'{diff_res:.3e}')
+                print(f'\n5: Corner peak:')
+                print(f'{result:.10e}')
+                print(f'{exact_res:.10e}')
+                print(f'{diff_res:.3e}')
 
-    # result = integral(fof.c0_fcn)
-    # exact_res = fof.exact_c0()
-    # print(f'\nResults: {result} {exact_res} {result - exact_res}')
-
-    # result = integral(fof.corner_peak_fcn)
-    # exact_res = fof.exact_corner_peak()
-    # print(f'\nResults: {result} {exact_res} {result - exact_res}')
-
-    # result = integral(fof.discontinuous_fcn)
-    # exact_res = fof.exact_discontinuous()
-    # print(f'\nResults: {result} {exact_res} {result - exact_res}')
+                f.write('   6:')
+                result = integral(fof.discontinuous_fcn)
+                exact_res = fof.exact_discontinuous()
+                diff_res = result - exact_res
+                f.write(f'{diff_res:.3e}')
+                print(f'\n6: Discontinuous:')
+                print(f'{result:.10e}')
+                print(f'{exact_res:.10e}')
+                print(f'{diff_res:.3e}')
 
     del integral
 
