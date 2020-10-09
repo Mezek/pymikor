ea4cd102ffba96cdd033b7777cb365696a78404f
Erik Bartoš <erik.bartos@gmail.com>
2020-10-09 12:28:03 +0200

    fix eps resolution

--- examples/test_methods.py
+++ examples/test_methods.py
@@ -12,28 +12,28 @@ import vegas
 
 
 def main():
-    ndim = 17
-    nods = 1009
+    ndim = 3
+    nods = 10009
     v_integ = vegas.Integrator(ndim * [[0, 1]])
     p_integ = PyMikor()
     p_integ.set_values(1, ndim, nods, 1, sigma=2)
-    p_integ.show_parameters()
     fof = Integrand('FCN', ndim)
     fof.normalize_a(5, 100)
 
-    # v_result = v_integ(fof.corner_peak_fcn)
-    # # v_result = v_integ(fof.corner_peak_fcn, nitn=10, neval=1e3)
-    # p_result = p_integ(fof.corner_peak_fcn, eps=1e-5)
+    v_result = v_integ(fof.oscillatory_fcn)
+    # v_result = v_integ(fof.oscillatory_fcn, nitn=10, neval=1e3)
+    p_result = p_integ(fof.oscillatory_fcn)  # , eps=1e-4
+    p_integ.show_parameters()
     # # c_result = cuba Divone
+    # exact_res = fof.exact_corner_peak()
     exact_res = fof.exact_oscillatory()
 
-    # v_rel_res = (v_result - exact_res) / exact_res
-    # p_rel_res = (p_result - exact_res) / exact_res
+    v_rel_res = (v_result - exact_res) / exact_res
+    p_rel_res = (p_result - exact_res) / exact_res
     # # c_rel_res = (c_result - exact_res) / exact_res
 
-    # print(f'\nResult of integration   : {exact_res}: {v_result} / {p_result}')
-    # print(f'\nRelative result         : {v_rel_res} / {p_rel_res}')
-    print(f'Res: {exact_res}')
+    print(f'\nResult of integration   : {exact_res}')
+    print(f'\nRelative result         : {v_rel_res} / {p_rel_res}')
 
     del p_integ
 
