0484c9728ede383f3cc0e98a97953841d56aca6b
Erik Bartoš <erik.bartos@gmail.com>
2020-10-25 13:21:15 +0100

    run oscillatory

--- examples/mthd_pymikor.py
+++ examples/mthd_pymikor.py
@@ -19,17 +19,19 @@ def format_num(f):
 
 
 def proceed_err(ndim, nods, nt):
+    r_vec = [nt, ndim]
     p_integ = PyMikor()
     fof = Integrand('FCN', ndim)
-    # fof.normalize_a(2, 100)
+    # fof.normalize_a(1.5, 110)
     exact_res = fof.exact_oscillatory()
     v_integ = vegas.Integrator(ndim * [[0, 1]])
-    v_result = v_integ(fof.oscillatory_fcn).mean
-    # v_result = v_integ(fof.oscillatory_fcn, nitn=10, neval=1e3).mean
-    v_rel_res = math.fabs((v_result - exact_res) / exact_res)
-    v_rel_res = format_num(v_rel_res)
+    v_result = []
+    v_result.append(v_integ(fof.oscillatory_fcn, nitn=10, neval=1e3).mean)
+    v_result.append(v_integ(fof.oscillatory_fcn, nitn=10, neval=1e5).mean)
+    for v in v_result:
+        v = math.fabs((v - exact_res) / exact_res)
+        r_vec.append(format_num(v))
 
-    r_vec = []
     for tab_prime in nods:
         p_integ.set_values(1, ndim, tab_prime, 1, sigma=2)
         p_result = p_integ(fof.oscillatory_fcn)  # , eps=1e-4
@@ -37,26 +39,28 @@ def proceed_err(ndim, nods, nt):
         p_rel_res = math.fabs((p_result - exact_res) / exact_res)
         r_vec.append(format_num(p_rel_res))
 
-    with open('data_pymikor.csv', 'a', newline='') as file:
+    with open('data_f_oscillatory.csv', 'a', newline='') as file:
         wr = csv.writer(file, delimiter=',', quoting=csv.QUOTE_ALL)
-        wr.writerow([nt, ndim, v_rel_res] + r_vec)
+        wr.writerow(r_vec)
 
     del p_integ, v_integ
 
 
 def main():
-    ndim = 4
+    ndim = [3, 5, 8, 10, 13, 15]
     nods = np.array([1259, 10007, 100003, 1000003])
 
-    with open('data_pymikor.csv', 'w', newline='') as file:
+    with open('data_f_oscillatory.csv', 'w', newline='') as file:
         wr = csv.writer(file, delimiter=',', quoting=csv.QUOTE_ALL)
-        header = ['A', 'NDim', 'Vegas']
+        header = ['A', 'NDim', 'VegasA', 'VegasB']
         for el in nods:
             header.append(el)
         wr.writerow(header)
 
-    for i in range(10):
-        proceed_err(ndim, nods, i+1)
+    for i in range(len(ndim)):
+        for j in range(1000):
+            # proceed_err(ndim[i], nods, j+1)
+            print(i, j+1, ndim[i], nods)
 
 
 if __name__ == "__main__":
